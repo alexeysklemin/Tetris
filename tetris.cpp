ea5bcfb55b1f5d10f6e3f02f85630fbdbac438e2
нет2 каби <a_sklemin@mail.ru>
2021-11-15 19:58:29 +0400

    new construction

--- tetris.cpp
+++ tetris.cpp
@@ -1,5 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <time.h>
+#include "figure.h"
 using namespace sf;
 
 const int M = 20;
@@ -7,21 +8,7 @@ const int N = 10;
 
 int field[M][N] = { 0 };
 
-struct Point
-{
-    int x, y;
-} a[4], b[4];
 
-int figures[7][4] =
-{
-    1,3,5,7, // I
-    2,4,5,7, // Z
-    3,5,4,6, // S
-    3,5,4,7, // T
-    2,3,5,7, // L
-    3,5,7,6, // J
-    2,3,4,5, // O
-};
 
 bool check()
 {
